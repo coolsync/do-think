3f9a4abb752488e167ab51ec032412e2c781b8b4
coolsync <coolsyn@outlook.com>
2021-04-18 15:08:26 +0800

    v3 apr 18

---
+++ GoSpace/GoGin_2021_3_11/vue_pro/02_vue3/vue-project/20_手写组合API/index.js
@@ -0,0 +1,136 @@
+// 1 shallowReactive 与 reactive
+const reactiveHandler = {
+    get(target, prop) {
+        // if (prop === '_is_reactive') return true
+        console.log('get method, read target data');
+        return Reflect.get(target, prop);
+    },
+
+    set(target, prop, value) {
+        const result = Reflect.set(target, prop, value)
+        console.log('set method, 数据已更新, 去更新界面')
+        return result
+    },
+
+    deleteProperty(target, prop) {
+        const result = Reflect.deleteProperty(target, prop)
+        console.log('deleteProperty method, 数据已删除, 去更新界面')
+        return result
+    },
+}
+
+// 自定义shallowReactive
+function shallowReactive(target) {
+    if (target && typeof target === 'object') {
+        return new Proxy(target, reactiveHandler)
+    }
+    return target
+}
+
+// 自定义reactive
+function reactive(target) {
+    if (target && typeof target === 'object') {
+        if (target instanceof Array) { // 数组
+            target.forEach((item, index) => {
+                target[index] = reactive(item)
+            })
+        } else { // 对象
+            Object.keys(target).forEach(key => {
+                target[key] = reactive(target[key])
+            })
+        }
+
+        const proxy = new Proxy(target, reactiveHandler)
+        return proxy
+    }
+
+    return target
+}
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//2 shallowReadonly 与 readonly
+const readonlyHandler = {
+    get(target, prop) {
+        if (prop === '_is_readonly') return true
+        console.log('get: read data', target, prop)
+        return Reflect.get(target, prop)
+    },
+
+    set() {
+        console.warn('set: 只读的, 不能修改')
+        return true
+    },
+
+    deleteProperty() {
+        console.warn('deleteProperty: 只读的, 不能删除')
+        return true
+    },
+}
+
+// 自定义shallowReadonly
+function shallowReadonly(target_obj) {
+    if (target_obj && typeof target_obj === 'object') {
+        return new Proxy(target_obj, readonlyHandler)
+    }
+    return target_obj
+}
+
+// 自定义readonly
+function readonly(target) {
+    if (target && typeof target === 'object') {
+        if (Array.isArray(target)) { // 数组
+            target.forEach((item, index) => {
+                target[index] = readonly(item)
+            })
+        } else { // 对象
+            Object.keys(target).forEach(key => {
+                target[key] = readonly(target[key])
+            })
+        }
+
+        const proxy = new Proxy(target, readonlyHandler)
+        return proxy
+    }
+
+    return target
+}
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// 3 shallowRef 与 ref
+// 自定义shallowRef
+function shallowRef(target) {
+    const result = {
+        _value: target, // 用来保存数据的内部属性
+        _is_ref: true, // 用来标识是ref对象
+        get value() {
+            console.log('Get value: data')
+            return this._value
+        },
+        set value(val) {
+            this._value = val
+            console.log('Set value 数据已更新, 去更新界面')
+        }
+    }
+    return result
+}
+
+// 自定义ref
+function ref(target) {
+    if (target && typeof target === 'object') {
+        target = reactive(target)
+    }
+    const result = {
+        _value: target, // 用来保存数据的内部属性
+        _is_ref: true, // 用来标识是ref对象
+        get value() {
+            console.log('Get value: data')
+            return this._value
+        },
+        set value(val) {
+            this._value = val
+            console.log('Set value 数据已更新, 去更新界面')
+        }
+    }
+    return result
+}
--- GoSpace/GoGin_2021_3_11/vue_pro/02_vue3/vue-project/src/App.vue
+++ GoSpace/GoGin_2021_3_11/vue_pro/02_vue3/vue-project/src/App.vue
@@ -1,49 +1,11 @@
 <template>
-  <h2>toRaw 与 markRaw</h2>
-  <h3>state: {{ state }}</h3>
-  <button @click="testToRaw">test toRaw</button>
-  <button @click="testMarkRaw">test markRaw</button>
+  <div></div>
 </template>
 
 <script lang='ts'>
-import { defineComponent, markRaw, reactive, toRaw } from "vue";
-
-interface UserInfo {
-  name: string;
-  age: number;
-  likes?: string[];
-}
+import {defineComponent} from 'vue'
 
 export default defineComponent({
-  name: "App",
-
-  setup() {
-    const state = reactive<UserInfo>({
-      name: "mark",
-      age: 25,
-    });
-
-    const testToRaw = () => {
-      const user = toRaw(state); // 这是一个还原方法，可用于临时读取，访问不会被代理/跟踪，写入时也不会触发界面更新
-      user.name += "++";
-      console.log(user.name);
-    };
-
-    const testMarkRaw = () => {
-      // state.likes = ["chi", "he"];
-      const likes = ["chi", "he"];
-      state.likes = markRaw(likes); // 标记一个对象，使其永远不会转换为代理。返回对象本身 // likes数组就不再是响应式的了
-      setInterval(() => {
-        // console.log(state.likes) // obj
-        state.likes[0] += "++";
-        console.log("----");
-      }, 1000);
-    };
-    return {
-      state,
-      testToRaw,
-      testMarkRaw,
-    };
-  },
-});
+  name: 'App',
+})
 </script>
