7cbbf3d2b9389abb40a4513057cba8c35ed2693e
coolsync <coolsyn@outlook.com>
2021-07-26 19:15:40 +0800

    rs

--- rust-main/hello/src/main.rs
+++ rust-main/hello/src/main.rs
@@ -3,9 +3,9 @@ use std::env;
 fn main() {
    let name = env::args().skip(1).next();
 
+
    match name {     // options
        Some(n) => println!("hi {}", n),
        None => println!("None")
    }
-
 }
